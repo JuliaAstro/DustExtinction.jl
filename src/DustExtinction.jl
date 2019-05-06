66c23ec284f31630f086fc2aa10a52f6378d5063
Miles Lucas <mdlucas@iastate.edu>
2019-05-06 07:54:09 -0500

    add convenience funciton and refactor ccm89 slightly

--- src/DustExtinction.jl
+++ src/DustExtinction.jl
@@ -1,7 +1,13 @@
 module DustExtinction
 
+# Convenience function for wavelength conversion
+function aa_to_invum(wave::Real)
+    return 1e4 / wave
+end
+
 # Extinction Laws
 include("ccm89.jl") # Also includes od94
-include("fm07.jl")
+include("cal00.jl")
 include("SFD98Map.jl")
+
 end
