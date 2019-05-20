2b382b0d5042f65d2ced6f9be67a8370f1318ffe
Miles Lucas <mdlucas@iastate.edu>
2019-05-20 18:03:41 -0500

    Added deprecation and test for deprecation, put od94 tests in correct file.

--- src/DustExtinction.jl
+++ src/DustExtinction.jl
@@ -10,4 +10,7 @@ include("ccm89.jl") # Also includes od94
 include("cal00.jl")
 include("SFD98Map.jl")
 
+@deprecate ccm89(x::AbstractArray) ccm89.(x::AbstractArray)
+@deprecate od94(x::AbstractArray) od94.(x::AbstractArray)
+
 end
