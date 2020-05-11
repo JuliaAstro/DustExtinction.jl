502ca1fc71286dcea8c9ff6ff78736200b0544a2
Miles Lucas <mdlucas@hawaii.edu>
2020-05-11 02:16:38 -1000

    add seperate deprecation file with annotations

---
+++ src/deprecate.jl
@@ -0,0 +1,12 @@
+# Here are all the deprecations
+
+# deprecations from 0.4 on
+@deprecate ccm89(x::AbstractArray, r_v::Real = 3.1) ccm89.(x, r_v)
+@deprecate od94(x::AbstractArray, r_v::Real = 3.1) od94.(x, r_v)
+
+# deprecations from 0.7 on
+@deprecate redden(f::Real, λ::Real, Av::Real; Rv = 3.1, law = ccm89) = redden(CCM89, λ, f; Av = Av, Rv = 3.1)
+@deprecate redden(f::Quantity, λ::Quantity, Av::Real; Rv = 3.1, law = ccm89) = redden(CCM89, λ, f; Av = Av, Rv = 3.1)
+
+@deprecate deredden(f::Real, λ::Real, Av::Real; Rv = 3.1, law = ccm89) = deredden(CCM89, λ, f; Av = Av, Rv = 3.1)
+@deprecate deredden(f::Quantity, λ::Quantity, Av::Real; Rv = 3.1, law = ccm89) = deredden(CCM89, λ, f; Av = Av, Rv = 3.1)
