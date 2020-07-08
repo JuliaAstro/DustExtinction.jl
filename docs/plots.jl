6eaa0aab3ed507f6d9613b582297894b2492274c
siddharthlal25 <siddharthlal25@gmail.com>
2020-07-09 05:12:41 +0530

    added plot

--- docs/plots.jl
+++ docs/plots.jl
@@ -1,5 +1,5 @@
 using Plots, LaTeXStrings
-import DustExtinction: ccm89_ca, ccm89_cb, od94_ca, od94_cb, cal00_invum, ccm89_invum, vcg04_invum, gcc09_invum, FM90
+import DustExtinction: ccm89_ca, ccm89_cb, od94_ca, od94_cb, cal00_invum, ccm89_invum, vcg04_invum, gcc09_invum, FM90, P92
 
 dir = joinpath(@__DIR__, "src", "assets")
 
@@ -102,3 +102,40 @@ plot!(w, m4, label = "FUV rise term")
 xlabel!(L"\mu m ^{-1}")
 ylabel!(L"E(\lambda - V)/E(B - V)")
 savefig(joinpath(dir, "FM90_plot.svg"))
+
+#--------------------------------------------------------------------------------
+# P92
+
+# plotting x in micrometers
+# wave numbers generated in mu-1
+w = exp10.(range(-3, 3, step = 0.001))
+
+# wave generated in angstrom
+x = 1e4 ./ w
+# size was modified to accomodate the tally table
+plot(size = (700, 500), xaxis = :log10, yaxis = :log10, xticks = ([0.001, 0.01, 0.1, 1, 10, 100, 1000]), yticks = ([0.001, 0.01, 0.1, 1, 10]))
+
+m1 = P92().(x)
+plot!(1 ./w, m1, label = "total")
+
+m2 = P92(FUV_amp = 0, NUV_amp = 0, SIL1_amp = 0, SIL2_amp = 0, FIR_amp = 0).(x)
+plot!(1 ./w, m2, label = "BKG only")
+
+m3 = P92(NUV_amp=0.0, SIL1_amp=0.0, SIL2_amp=0.0, FIR_amp=0.0).(x)
+plot!(1 ./w, m3, label = "BKG+FUV only")
+
+m4 = P92(FUV_amp=0.0, SIL1_amp=0.0, SIL2_amp=0.0, FIR_amp=0.0).(x)
+plot!(1 ./w, m4, label = "BKG+NUV only")
+
+m5 = P92(FUV_amp = 0, NUV_amp = 0, SIL2_amp = 0).(x)
+plot!(1 ./w, m5, label = "BKG+FIR+SIL1 only")
+
+m6 = P92(FUV_amp=0., NUV_amp=0.0, SIL1_amp=0.0).(x)
+plot!(1 ./w, m6, label = "BKG+FIR+SIL2 only")
+
+m7 = P92(FUV_amp=0., NUV_amp=0.0, SIL1_amp=0.0, SIL2_amp=0.0).(x)
+plot!(1 ./w, m7, label = "BKG+FIR only")
+
+xlabel!(L"x\ \left[\mu m\right]")
+ylabel!(L"A(X)/A(V)")
+savefig(joinpath(dir, "P92_plot.svg"))
