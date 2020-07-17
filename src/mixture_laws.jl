4322388d5983c43817fb5d76d20e6cdab107d040
Ian Weaver <iweaver@cfa.harvard.edu>
2020-07-17 02:05:09 -0400

    Update src/mixture_laws.jl
    Co-authored-by: Miles Lucas <mdlucas@hawaii.edu>

--- src/mixture_laws.jl
+++ src/mixture_laws.jl
@@ -86,7 +86,6 @@ extinction. This is mixture model between the F99 R(V) dependent model
 (component A) and the [`G03_SMCBar`](@ref) model (component B) The default support is
 [1000, 33333] Å. Outside of that range this will return 0. Rv is the selective
 extinction and is valid over [2, 6]. A typical value for the Milky Way is 3.1.
-By default, Rv=3.1 and f_A=1.0 is used.
 
 # References
 [Gordon et al. (2016)](https://ui.adsabs.harvard.edu/abs/2016ApJ...826..104G/)
