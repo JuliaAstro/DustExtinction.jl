3b80732ea9b9c30114d15d0f8ef53d746a53a83e
William Thompson <will.thompson@outlook.com>
2022-08-26 13:23:27 -0700

    Towards unified JuliaAstro docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,16 +2,12 @@ using Documenter
 using DustExtinction
 
 DocMeta.setdocmeta!(DustExtinction, :DocTestSetup, :(using DustExtinction); recursive = true)
-
+include("pages.jl")
 makedocs(modules = [DustExtinction],
     sitename = "DustExtinction.jl",
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
     authors = "Kyle Barbary, Mosé Giordano, Miles Lucas",
-    pages = [
-        "Home" => "index.md",
-        "Color Laws" => "color_laws.md",
-        "Dust Maps" => "dust_maps.md"
-    ],
+    pages = pages
 )
 
 deploydocs(repo = "github.com/JuliaAstro/DustExtinction.jl.git", push_preview = true)
---
+++ docs/pages.jl
@@ -0,0 +1,5 @@
+pages = [
+    "Home" => "index.md",
+    "Color Laws" => "color_laws.md",
+    "Dust Maps" => "dust_maps.md"
+]
