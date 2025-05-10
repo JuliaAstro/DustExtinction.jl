92e890c62e783b31a76f1a1fd89f422addd34cc7
abhro <5664668+abhro@users.noreply.github.com>
2025-05-10 12:04:32 -0400

    Add doc site favicon

--- docs/make.jl
+++ docs/make.jl
@@ -3,14 +3,19 @@ using DustExtinction
 
 DocMeta.setdocmeta!(DustExtinction, :DocTestSetup, :(using DustExtinction); recursive = true)
 include("pages.jl")
-makedocs(modules = [DustExtinction],
+
+makedocs(;
+    modules = [DustExtinction],
     sitename = "DustExtinction.jl",
     format = Documenter.HTML(;
         prettyurls = get(ENV, "CI", nothing) == "true",
         edit_link = "master",
+        assets = [
+            "assets/favicon.ico",
+        ],
     ),
     authors = "Kyle Barbary, Mosé Giordano, Miles Lucas",
-    pages = pages,
+    pages,
     warnonly = [:missing_docs],
 )
 
